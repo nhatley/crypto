f531b2fa35d7e4ee67dd07441c3fb78a3ce42859
nhatley <nhatley@gmail.com>
2018-03-03 23:03:35 -0500

    Who knows

--- code/get_historical_data.R
+++ code/get_historical_data.R
@@ -1,58 +1,43 @@
 library(tidyverse)
 library(httr)
 
-current_date <- as.numeric(as.POSIXct(Sys.Date(), format="%Y-%m-%d"))
-  
-six_months_ago <- as.POSIXct(Sys.Date(), format="%Y-%m-%d") - lubridate::dweeks(24)
-
 #documenation here: https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10
 
 #all comes from this base: https://rest.coinapi.io/v1/ohlcv/
-
-#coin API
-#site = https://docs.coinapi.io/#general
-key = "F21A43CA-D364-47CA-B7D4-11CB7A0BD205"
-url_base = "https://rest.coinapi.io/"
-middle = "/v1/ohlcv/BTC"
-period = "?period_id="
-
-bitstamp_btc_usd = "BITSTAMP_SPOT_BTC_USD"
-bitstamp_eth_usd = "BITSTAMP_SPOT_ETH_USD"
-bitstamp_ltc_usd = "BITSTAMP_SPOT_LTC_USD"
-bitstamp_xrp_usd = "BITSTAMP_SPOT_XRP_USD"
-bitstamp_bch_usd = "BITSTAMP_SPOT_BCH_USD"
-
-#can see all symbols at data/lists/symbols.rds
-coins = list(bitstamp_btc_usd, bitstamp_eth_usd, bitstamp_ltc_usd, 
-             bitstamp_xrp_usd, bitstamp_bch_usd)
-saveRDS(coins, "data/lists/five_coins.rds")
+coins = read_rds("data/lists/five_coins.rds")
 #all historical functions live here
 source("code/functions/get_historical.R")
 #one day
+one_day = map_df(coins, function(x) get_historical_days(x, 1)) %>% mutate(duration = "one_day")
+saveRDS(one_day, "data/historical/time_series/btc_eth_ltc_xrp_bch_last_day.rds")
+#two dayss
+#five days
+five_days = map_df(coins, function(x) get_historical_days(x, 5)) %>% mutate(duration = "five_days")
+saveRDS(five_days, "data/historical/time_series/btc_eth_ltc_xrp_bch_five_days.rds")
+#one week
 one_week = map_df(coins, function(x) get_historical_weeks(x, 1)) %>% mutate(duration = "one_week")
-saveRDS(six_months, "data/historical/btc_eth_ltc_xrp_bch_sixmos.rds")
-
-
-one_week = map_df(coins, function(x) get_historical_weeks(x, 1)) %>% mutate(duration = "one_week")
-saveRDS(six_months, "data/historical/btc_eth_ltc_xrp_bch_sixmos.rds")
-
-
+saveRDS(one_week, "data/historical/time_series/btc_eth_ltc_xrp_one_week.rds")
 #one month
-six_months = map_df(coins, get_one_months)
-saveRDS(six_months, "data/historical/btc_eth_ltc_xrp_bch_sixmos.rds")
-  #six months
-six_months = map_df(coins, get_six_months)
-saveRDS(six_months, "data/historical/btc_eth_ltc_xrp_bch_sixmos.rds")
+one_month = map_df(coins, function(x) get_historical_weeks(x, 4)) %>% mutate(duration = "one_month")
+saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_one_month.rds")
+#three_months #this is the furhest back BITSTAMP_SPOT_BCH gpoes
+three_months = map_df(coins, 
+                      function(x) get_historical_weeks(x, 12)) %>% mutate(duration = "three_months")
+saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_one_month.rds")
+#six months
+six_months = map_df(coins, 
+                    function(x) get_historical_weeks(x, 24)) %>% mutate(duration = "six_months")
+saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_six_months.rds")
+#nine months
+nine_months = map_df(coins, 
+                    function(x) get_historical_weeks(x, 36)) %>% mutate(duration = "nine_months")
+saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_nine_months.rds")
+#one_year furthest back for BITSTAMP_SPOT_ETH_USD, BITSTAMP_SPOT_LTC_USD
+one_year = map_df(coins, 
+                    function(x) get_historical_weeks(x, 48)) %>% mutate(duration = "one_year")
+saveRDS(one_year, "data/historical/time_series/btc_eth_ltc_xrp_bch_one_year.rds")
 
-  #one_year
-one_year = map_df(coins, get_one_year)
-#some symbols are not available this long; investigate
-saveRDS(one_year, "data/historical/btc_eth_ltc_xrp_bch_oneyear.rds")
+all_data = one_day %>% bind_rows(six_months) %>% bind_rows(nine_months) %>% 
+  bind_rows(one_year) %>% bind_rows(two_years)
 
-  #two_years
-two_years = map_df(coins, get_two_years)
-#some symbols are not available this long; investigate
-saveRDS(one_year, "data/historical/btc_eth_ltc_xrp_bch_twoyears.rds")
-
-
-
+saveRDS(all_data, "data/historical/btc_eth_ltc_xrp_bch.rds")
