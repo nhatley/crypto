52ab29812be238a516890ce460e2a145e965609d
nhatley <nhatley@gmail.com>
2018-03-04 20:39:25 -0500

    Updated get_historical_data and data storage file paths

--- code/get_historical_data.R
+++ code/get_historical_data.R
@@ -7,37 +7,35 @@ library(httr)
 coins = read_rds("data/lists/five_coins.rds")
 #all historical functions live here
 source("code/functions/get_historical.R")
-#one day
-one_day = map_df(coins, function(x) get_historical_days(x, 1)) %>% mutate(duration = "one_day")
-saveRDS(one_day, "data/historical/time_series/btc_eth_ltc_xrp_bch_last_day.rds")
-#two dayss
-#five days
-five_days = map_df(coins, function(x) get_historical_days(x, 5)) %>% mutate(duration = "five_days")
-saveRDS(five_days, "data/historical/time_series/btc_eth_ltc_xrp_bch_five_days.rds")
 #one week
 one_week = map_df(coins, function(x) get_historical_weeks(x, 1)) %>% mutate(duration = "one_week")
-saveRDS(one_week, "data/historical/time_series/btc_eth_ltc_xrp_one_week.rds")
+saveRDS(one_week, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_one_week", "_", Sys.Date(), ".rds"))
 #one month
 one_month = map_df(coins, function(x) get_historical_weeks(x, 4)) %>% mutate(duration = "one_month")
-saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_one_month.rds")
+saveRDS(one_month, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_one_month", "_", Sys.Date(), ".rds"))
 #three_months #this is the furhest back BITSTAMP_SPOT_BCH gpoes
 three_months = map_df(coins, 
                       function(x) get_historical_weeks(x, 12)) %>% mutate(duration = "three_months")
-saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_one_month.rds")
+saveRDS(three_months, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_three_months", "_", Sys.Date(), ".rds"))
 #six months
 six_months = map_df(coins, 
                     function(x) get_historical_weeks(x, 24)) %>% mutate(duration = "six_months")
-saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_six_months.rds")
+saveRDS(six_months, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_six_months", "_", Sys.Date(), ".rds"))
 #nine months
 nine_months = map_df(coins, 
                     function(x) get_historical_weeks(x, 36)) %>% mutate(duration = "nine_months")
-saveRDS(six_months, "data/historical/time_series/btc_eth_ltc_xrp_bch_nine_months.rds")
+saveRDS(nine_months, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_nine_months", "_", Sys.Date(), ".rds"))
 #one_year furthest back for BITSTAMP_SPOT_ETH_USD, BITSTAMP_SPOT_LTC_USD
 one_year = map_df(coins, 
                     function(x) get_historical_weeks(x, 48)) %>% mutate(duration = "one_year")
-saveRDS(one_year, "data/historical/time_series/btc_eth_ltc_xrp_bch_one_year.rds")
+saveRDS(one_year, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_one_year", "_", Sys.Date(), ".rds"))
+two_years = map_df(coins, 
+                  function(x) get_historical_weeks(x, 96)) %>% mutate(duration = "two_years")
+saveRDS(one_year, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp_two_years", "_", Sys.Date(), ".rds"))
 
-all_data = one_day %>% bind_rows(six_months) %>% bind_rows(nine_months) %>% 
+all_data = one_week %>% bind_rows(one_month) %>% bind_rows(three_months) %>% 
+  bind_rows(six_months) %>%  bind_rows(nine_months) %>% 
   bind_rows(one_year) %>% bind_rows(two_years)
 
-saveRDS(all_data, "data/historical/btc_eth_ltc_xrp_bch.rds")
+saveRDS(all_data, "data/historical/all_data.rds")
+saveRDS(all_data, paste0("data/historical/time_series_cache//btc_eth_ltc_xrp", "_", Sys.Date(), ".rds"))
